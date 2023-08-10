1d0f680db36b569ff46dab0fb24469f546b84073
russiantech <chrisjsmez@gmail.com>
2023-08-10 10:04:10 -0700

    3-print_alphabt.py syntax errors fixed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 #Print all the letters except q and e
 for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(letter) != 'q' and chr(letter) !=  'e':
         print("{}".format(chr(letter)), end="")
