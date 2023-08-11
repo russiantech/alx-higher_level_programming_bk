ec0b75c1a5a291e7bc9fcbe3a5ca2d08e491536c
russiantech <chrisjsmez@gmail.com>
2023-08-11 03:08:33 -0700

    high-level-but-simple hello world programs with python

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+# magi_calculation does same thing as the commented block below
+""" 3           0 LOAD_CONST               1 (98)
+ 3 LOAD_FAST                0 (a)
+ 6 LOAD_FAST                1 (b)
+ 9 BINARY_POWER
+10 BINARY_ADD
+3           0 LOAD_CONST               1 (98)
+ 3 LOAD_FAST                0 (a)
+ 6 LOAD_FAST                1 (b)
+ 9 BINARY_POWER
+10 BINARY_ADD
+11 RETURN_VALUE 11 RETURN_VALUE
+ """
+
+
 def magic_calculation(a, b):
     return (98 + a ** b)
