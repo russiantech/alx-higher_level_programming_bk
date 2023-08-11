abe432d75d1fd90ebd4149aa11fcf5d0b428013f
russiantech <chrisjsmez@gmail.com>
2023-08-10 22:22:24 -0700

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ magic_calculation(a, b): that does exactly as
+ Python bytecode in the question
+"""
+
+def magic_calculation(a, b):
+ if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,4 @@
+ #!/usr/bin/python3
+alx-higher_level_programming
+
+Chris James,student 2023
