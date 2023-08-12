4decbe2407bfa01aeb1982b2a4577e31e6618bf8
russiantech <chrisjsmez@gmail.com>
2023-08-12 03:18:25 -0700

    failed pycode style requirements fixed( 2-args.py)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ import sys
 if __name__ == "__main__":
     count = len(sys.argv) - 1
     if count == 1:
-        #print("1 argument:")
-        #print("1: {}".format(sys.argv[1]))
+        # print("1 argument:")
+        # print("1: {}".format(sys.argv[1]))
         print("1 argument:\n1: {}".format(sys.argv[1]))
     elif count == 0:
         print("0 arguments.")
