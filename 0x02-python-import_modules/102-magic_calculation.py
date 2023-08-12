1abd21c78fe7f9ec42287c3059626b7e27da6841
russiantech <chrisjsmez@gmail.com>
2023-08-12 04:32:51 -0700

    import error fixed

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
+from calculator_1 import add, sub
 """
  magic_calculation(a, b): that does exactly as
  Python bytecode in the question
