3cde9cbcfc501ca808929d1cab61b2cda05a1ef7
russiantech <chrisjsmez@gmail.com>
2023-10-19 06:42:27 -0700

    Advanced Task - 0x0E. SQL - More queries

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- get genres from/in our hbtn_0d_tvshows_rate by their rating
+-- get rows in a db table linked to a row in another table
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name ORDER BY rating DESC;
