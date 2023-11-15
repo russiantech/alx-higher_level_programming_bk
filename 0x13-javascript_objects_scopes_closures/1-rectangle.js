54668f31eca86abc4cb690e0d4e2065dc1708d82
russiantech <chrisjsmez@gmail.com>
2023-11-15 05:36:03 -0800

    1-rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 class Rectangle {
-
   constructor (w, h) {
 
     this.width = w;
@@ -9,7 +8,6 @@ class Rectangle {
     this.height = h;
 
   }
-
 }
 
 module.exports = Rectangle;
