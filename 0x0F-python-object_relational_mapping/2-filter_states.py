6620613223fa3a546d955ed45081ff3b80ec4213
russiantech <chrisjsmez@gmail.com>
2023-11-18 22:52:02 -0800

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/2-filter_states.py
+++ 0x0F-python-object_relational_mapping/2-filter_states.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 
-"""Get * states from hbtn_0e_0_usa DB & filter it """
-
+"""lists states having passed args hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE\
-            BINARY '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
