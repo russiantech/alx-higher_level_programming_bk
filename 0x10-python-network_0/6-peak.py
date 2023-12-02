d3632517bb4469e7faa5add659fa90b88a3cbf9c
russiantech <chrisjsmez@gmail.com>
2023-12-02 04:02:02 -0800

    hot fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
-""" find the max """
+"""
+Module for finding a peak in a list of unsorted integers.
+"""
+
 
 def find_peak(list_of_integers):
-    """find_peak"""
-    if not len(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+
+    Args:
+    - list_of_integers: A list of unsorted integers.
+
+    Returns:
+    - The peak integer.
+    """
+
+    if not list_of_integers:
         return None
-    else:
-        return max(list_of_integers)
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
