1003afc80b091904306f366bcdb4ecc44e9cf915
russiantech <chrisjsmez@gmail.com>
2023-12-01 22:54:29 -0800

    Fix on All your scripts should be exactly 3 lines long (wc -l file should print 3)

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Takes a URL, sends a request,& displays the size of the response body
-
-#curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
 curl -so /dev/null "$1" -w '%{size_download}\n'
